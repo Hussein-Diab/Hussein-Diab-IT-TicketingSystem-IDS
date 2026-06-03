3fcdd7dadd5f3b1f90659f08d5c5edd0cb677224
Hussein-Diab <hsendiab21@gmail.com>
2026-06-03 20:26:26 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
-// Show login page
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 
-// Handle login form submission
 Route::post('/login', [AuthController::class, 'login']);
 
-// Logout
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
